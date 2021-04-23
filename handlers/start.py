be8112d6fbaf80d869f2c05f0afbc547e77ab85e
Pawan JATT <76043107+jattpawan@users.noreply.github.com>
2021-04-23 12:16:35 +0530

    Add files via upload

---
+++ handlers/start.py
@@ -0,0 +1,42 @@
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import BOT_NAME as bn
+from helpers.filters import other_filters2
+
+
+@Client.on_message(other_filters2)
+async def start(_, message: Message):
+    await message.reply_text(
+        f"""I am **{bn}** !!
+I let you play music in your group's voice chat 😉
+The commands I currently support are:
+⚜️ /play - __Plays the replied audio file or YouTube video through link.__
+⚜️ /pause - __Pause Voice Chat Music.__
+⚜️ /resume - __Resume Voice Chat Music.__
+⚜️ /skip - __Skips the current Music Playing In Voice Chat.__
+⚜️ /stop - __Clears The Queue as well as ends Voice Chat Music.__
+        """,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "📔 Source Code 📔", url="https://github.com/Hexor752/sankiimusicbot"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "💬 Group 💬", url="https://t.me/SankiiPublic"
+                    ),
+                    InlineKeyboardButton(
+                        "📣 Owner 📣", url="https://t.me/Its_Hexor"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "❌ Close ❌", callback_data="close"
+                    )
+                ]
+            ]
+        )
+    )
